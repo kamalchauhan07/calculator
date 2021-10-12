9bc45edc79728620ac3759775dfc56f9bedd1f13
bhawnachauhan07 <92302182+bhawnachauhan07@users.noreply.github.com>
2021-10-12 11:39:52 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -132,6 +132,28 @@ class TicTacToe:
             if winner != 0:
                 break
     return(winner)
+def create_board():
+    return(np.array([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]]))
+  
+# Check for empty places on board
+def possibilities(board):
+    l = []
+      
+    for i in range(len(board)):
+        for j in range(len(board)):
+              
+            if board[i][j] == 0:
+                l.append((i, j))
+    return(l)
+  
+# Select a random place for the player
+def random_place(board, player):
+    selection = possibilities(board)
+    current_loc = random.choice(selection)
+    board[current_loc] = player
+    return(board)
 
            
 
